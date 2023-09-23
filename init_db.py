3b5ad74be190e5bf80dc0227f89e111c99c316c8
Venkat Nelluri <nvenkat427@gmail.com>
2023-09-23 12:47:12 -0500

    first commit

---
+++ init_db.py
@@ -0,0 +1,39 @@
+import sqlite3
+from datetime import datetime
+
+global database_nm 
+database_nm = "travel_db.db" #check this file in sql lite studio to query data
+
+def find_user_login(username):
+    connection = sqlite3.connect(database_nm)
+    cur = connection.cursor()
+    sql_query = 'SELECT distinct password FROM users where username = "' + username + '";'
+    t = cur.execute(sql_query).fetchall()
+    return str("".join(t[0]))
+    
+def select_all_users():
+    connection = sqlite3.connect(database_nm)
+    cur = connection.cursor()
+    all_users = cur.execute("select * from users").fetchall()
+    return [user for user in all_users]
+
+def create_table():
+   connection = sqlite3.connect(database_nm) 
+   with open('schema.sql') as f:
+        connection.executescript(f.read())
+    
+def insert_query_user(username, email, password, fname, lname):
+    connection = sqlite3.connect(database_nm)
+    cur = connection.cursor()
+    cur.execute("INSERT INTO users (username, email, password, firstname, lastname) VALUES (?, ?, ?, ?, ?)",
+                    (username, email, password, fname, lname))
+    connection.commit()
+    return "Record Inserted Successfully"
+
+def log_user_session(username, session_id):
+    connection = sqlite3.connect(database_nm)
+    cur = connection.cursor()
+    sql_statement = f"UPDATE user_sessions SET logout_time = CURRENT_TIMESTAMP, session_id = '{session_id}' WHERE username='{username}'"   
+    cur.execute(sql_statement)
+    connection.commit()
+    connection.close()
